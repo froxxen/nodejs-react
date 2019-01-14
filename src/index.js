3cf469c52589b7aec22ee37214493bef4734ee23
unknown <strom.christian@gmail.com>
2019-01-14 18:49:31 +0200

    Small modifications to index.js

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ const Hello = (props) => {
 
 const Footer = () => {
   return (
-    <div>greeting app created by <a href="https://github.com/mluukkai">mluukkai</a></div>
+    <div>Greeting app created by Froxxen</div>
   )
 }
 
 const App = () => {
   const nimi = 'Pekka'
-  const ika = 10
+  const ika = 101
   return (
     <div>
       <h1>Greetings</h1>
